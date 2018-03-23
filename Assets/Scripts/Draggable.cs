d6b97fdc2881c85106738ee401fdd015af520def
Will <will.lucen4@gmail.com>
2018-03-23 16:36:30 -0300

    init files and create a draggable script

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
+{
+	private float offsetX;
+	private float offsetY;
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log("begun");
+		offsetX = transform.position.x - eventData.position.x;
+		offsetY = transform.position.y - eventData.position.y;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Vector2 newPos = new Vector2(eventData.position.x + offsetX, eventData.position.y + offsetY);
+		this.transform.position = newPos;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("ended");
+	}
+}
