efb47698471123c4e929ef54ab1ab06bf7441e2d
Will <williamaster1@gmail.com>
2018-03-24 17:34:54 -0300

    scriptableCard, cardScript created and cardPrefab modified

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour
+{
+    public Card cardInfos;
+
+    [SerializeField] private Text cardName;
+    [SerializeField] private Text cardDescription;
+    [SerializeField] private Image cardImage;
+    [SerializeField] private Text cardAtk;
+    [SerializeField] private Text cardDef;
+    [SerializeField] private Text cardHealth;
+
+    void Start()
+    {
+        cardName.text = cardInfos.name;
+        cardDescription.text = cardInfos.description;
+        cardImage.sprite = cardInfos.image;
+        cardAtk.text = cardInfos.atk.ToString();
+        cardDef.text = cardInfos.def.ToString();
+        cardHealth.text = cardInfos.health.ToString();
+    }
+}
