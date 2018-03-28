291b38d633de3de674bf1945658914c19aa24b4a
Will <williamaster1@gmail.com>
2018-03-28 13:57:07 -0300

    Pushing a random card to hand

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Card[] CARDS;
+    [SerializeField] private GameObject cardPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void pushCard(Transform parent)
+    {
+        GameObject card = Instantiate(cardPrefab) as GameObject;
+        card.GetComponent<CardScript>().cardInfos = CARDS[Random.Range(0, CARDS.Length)];
+        card.transform.SetParent(parent);
+
+    }
+}
