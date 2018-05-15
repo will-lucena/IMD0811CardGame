2c53a46b743ebc5b5204f8decc52eefd73f01bd6
Will <williamaster1@gmail.com>
2018-05-15 17:37:10 -0300

    Adicionada ferramenta de criacao de cartas e cartas geradas por ela

---
+++ Assets/Editor/Datas/CardData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class CardData : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite image;
+    public int atk;
+    public int def;
+
+    public CardCreatorWindow.Type type;
+}
---
+++ Assets/Editor/Datas/HeroData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Hero", menuName = "Cards/Hero")]
+public class HeroData : CardData
+{
+    public int health;
+}
